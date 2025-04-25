135719209f58c4db4cc2e1f91c8a5d8d530761cd
Aaron <aaron98rg@gmail.com>
2025-04-25 17:59:06 +0200

    He cambiado la forma de crear productos haciendo una funcion fuera del main(agregar_producto) en vez de pedirlo en el case 1, 2 y 3

--- entregale.py
+++ entregale.py
@@ -6,19 +6,24 @@ class Productos:
 
     def agregar_producto(self, lista):
         lista.append(self)
-
-
-class Alamcen:
-    def __init__(self):
-        self.lista_frescos = []
-        self.lista_refrigerados =  []
-        self.lista_congelados = []
-    def mostrar_productos(self, lista):
-        return "\n".join(str(producto) for producto in lista)
-
+    
     def __str__(self):
         return f"Nombre: {self.nombre}, Fecha de caducidad: {self.fecha_caducidad}, Número de lote: {self.numero_lote}"
 
+    
+
+
+class Alamcen:
+    def __init__(self,lista_frescos = [], lista_refigerados = [], lista_congelados = []):
+        self.lista_frescos = lista_frescos
+        self.lista_refrigerados = lista_refigerados
+        self.lista_congelados = lista_congelados
+
+    def mostrar_productos(self, lista):
+        for producto in lista:
+            print(producto)
+
+    
 
 class Frescos(Productos):
     def __init__(self, nombre, fecha_caducidad, numero_lote, fecha_envasado, pais_origen):
@@ -29,7 +34,6 @@ class Frescos(Productos):
     def __str__(self):
         return super().__str__() + f", Fecha de envasado: {self.fecha_envasado}, País de origen: {self.pais_origen}"
 
-
 class Refrigerados(Productos):
     def __init__(self, nombre, fecha_caducidad, numero_lote, codigo_organismo):
         super().__init__(nombre, fecha_caducidad, numero_lote)
@@ -37,7 +41,6 @@ class Refrigerados(Productos):
 
     def __str__(self):
         return super().__str__() + f", Código del organismo: {self.codigo_organismo}"
-
 
 class Congelados(Productos):
     def __init__(self, nombre, fecha_caducidad, numero_lote, temperatura_recomendada):
@@ -47,13 +50,11 @@ class Congelados(Productos):
     def __str__(self):
         return super().__str__() + f", Temperatura recomendada: {self.temperatura_recomendada}"
 
-
 def solicitar_datos_comunes():
     nombre = input("Nombre del producto: ")
     fecha_caducidad = input("Fecha de caducidad: ")
     numero_lote = input("Número de lote: ")
     return nombre, fecha_caducidad, numero_lote
-
 
 def agregar_producto(tipo, almacen):
     nombre, fecha_caducidad, numero_lote = solicitar_datos_comunes()
@@ -74,10 +75,10 @@ def agregar_producto(tipo, almacen):
 
 
 def main():
-    almacen = Alamcen(lista_frescos=[], lista_congelados=[], lista_refrigerados=[])
+    almacen = Alamcen(lista_frescos=[], lista_congelados= [], lista_refigerados= [])
 
     while True:
-        print("\nMenu de gestion de productos:")
+        print("\nMenú de gestión de productos:")
         print("1. Agregar producto fresco")
         print("2. Agregar producto refrigerado")
         print("3. Agregar producto congelado")
@@ -86,7 +87,7 @@ def main():
         print("6. Mostrar productos congelados")
         print("7. Salir")
 
-        opcion = int(input("Seleccione una opcion: "))
+        opcion = int(input("Seleccione una opción: "))
 
         match opcion:
             case 1:
@@ -98,8 +99,9 @@ def main():
             case 3:
                 agregar_producto("congelado", almacen)
 
+
             case 4:
-                print(almacen.mostrar_productos(almacen.lista_frescos))
+                almacen.mostrar_productos(almacen.lista_frescos)
 
             case 5:
                 print(almacen.mostrar_productos(almacen.lista_refrigerados))
@@ -108,11 +110,9 @@ def main():
                 print(almacen.mostrar_productos(almacen.lista_congelados))
 
             case 7:
-                print("Saliendo del prgrama...")
+                print("Saliendo del programa.")
                 break
 
             case _:
-                print("Opcion no valida. Por favor, seleccione una opcin del 1 al 7.")
-
-
+                print("Opción no válida. Por favor, seleccione una opción del 1 al 7.")
 main()
